7b22f04cbedf0fc3d092be8ed3f6dcf1a09541bd
Meng-Gen Tsai <menggen@yahoo-inc.com>
2018-07-02 14:37:04 +0800

    DP with DP

--- problems/ACMAKER/main.cc
+++ problems/ACMAKER/main.cc
@@ -13,36 +13,37 @@ std::vector<std::string> Split(const std::string& content) {
     return tokens;
 }
 
-int Get(const std::string acronym, const std::vector<std::string>& words) {
-    // dp[i][j] is the number of ways to use the first i words and the
-    // first j letters of the acronym
-    int dp[151][151] = {0};
-    dp[0][0] = 1;
-    int dp2[151][151];
+int GetCount(const std::string& acronym, const std::vector<std::string>& words) {
+    // count[i][j] is the number of ways to use the first i words and the
+    // first j letters of the acronym.
+    int count[151][151] = {0};
+    count[0][0] = 1;
+
+    int count_in_word[151][151];
     for (int i = 0; i < words.size(); i++) {
         for (int j = 0; j < acronym.length(); j++) {
             // How many letters of this word do we want to use?
-            int mx = std::min(acronym.length() - j, words[i].length());
+            int max_in_word = std::min(acronym.length() - j, words[i].length());
             for (int k = 0; k <= words[i].length(); k++) {
-                dp2[k][0] = 1;
+                count_in_word[k][0] = 1;
             }
-            for (int k = 1; k <= mx; k++) {
-                dp2[0][k] = 0;
+            for (int k = 1; k <= max_in_word; k++) {
+                count_in_word[0][k] = 0;
             }
             for (int k = 0; k < words[i].length(); k++) {
-                for (int m = 0; m < mx; m++) {
-                    dp2[k+1][m+1] = dp2[k][m+1];
+                for (int m = 0; m < max_in_word; m++) {
+                    count_in_word[k+1][m+1] = count_in_word[k][m+1];
                     if (words[i][k] == tolower(acronym[j+m])) {
-                        dp2[k+1][m+1] += dp2[k][m];
+                        count_in_word[k+1][m+1] += count_in_word[k][m];
                     }
                 }
             }
-            for (int k = 1; k <= mx; k++) {
-                dp[i+1][j+k] += dp[i][j] * dp2[words[i].length()][k];
+            for (int k = 1; k <= max_in_word; k++) {
+                count[i+1][j+k] += count[i][j] * count_in_word[words[i].length()][k];
             }
         }
     }
-    return dp[words.size()][acronym.length()];
+    return count[words.size()][acronym.length()];
 }
 
 int main(int argc, char* argv[]) {
@@ -73,11 +74,11 @@ int main(int argc, char* argv[]) {
                     significant_words.push_back(all_words[i]);
                 }
             }
-            int ways_count = Get(acronym, significant_words);
-            if (ways_count == 0) {
+            int count = GetCount(acronym, significant_words);
+            if (count == 0) {
                 std::cout << acronym << " is not a valid abbreviation" << std::endl;
             } else {
-                std::cout << acronym << " can be formed in " << ways_count << " ways" << std::endl;
+                std::cout << acronym << " can be formed in " << count << " ways" << std::endl;
             }
         } 
     }
