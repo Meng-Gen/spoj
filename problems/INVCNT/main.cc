29a0ecee84c88d1dac8ae7c54bd1e6030cb7f08a
Meng-Gen Tsai <menggen@yahoo-inc.com>
2018-07-04 15:55:20 +0800

    BIT

--- problems/INVCNT/main.cc
+++ problems/INVCNT/main.cc
@@ -6,37 +6,30 @@
 
 long long a[MAX_LENGTH];
 long long compressed[MAX_LENGTH];
+long long tree[MAX_LENGTH];
 
-class BinaryIndexedTree {
-private:
-    long long tree[MAX_LENGTH];
-
-public:
-    BinaryIndexedTree() {
-        memset(tree, 0LL, sizeof(tree));
+long long Sum(int k) {
+    long long s = 0;
+    while (k >= 1) {
+        s += tree[k];
+        k -= k & (-k);
     }
+    return s;
+}
 
-    long long Sum(int k) {
-        long long s = 0;
-        while (k >= 1) {
-            s += tree[k];
-            k -= k & (-k);
-        }
-        return s;
+void Add(int k, int x) {
+    while (k <= MAX_LENGTH) {
+        tree[k] += x;
+        k += k & (-k);
     }
-
-    void Add(int k, int x) {
-        while (k <= MAX_LENGTH) {
-            tree[k] += x;
-            k += k & (-k);
-        }
-    }
-};
+}
 
 int main(int argc, char* argv[]) {
     int testcases;
     std::cin >> testcases;
     while (testcases--) {
+        memset(tree, 0LL, sizeof(tree)); // initialize binary indexed tree
+
         int n;
         std::cin >> n;
         for (int i = 0; i < n; i++) {
@@ -45,13 +38,11 @@ int main(int argc, char* argv[]) {
         }
         std::sort(compressed, compressed + n); // compress array
 
-        BinaryIndexedTree tree;
-
         long long count = 0;
         for (int i = 0; i < n; i++) {
             int pos = std::lower_bound(compressed, compressed + n, a[i]) - compressed;
-            count += tree.Sum(n + 1) - tree.Sum(pos + 1); // binary indexed tree is 1-based
-            tree.Add(pos + 1, 1);
+            count += Sum(n + 1) - Sum(pos + 1); // binary indexed tree is 1-based
+            Add(pos + 1, 1);
         }
 
         std::cout << count << std::endl;
